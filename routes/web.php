36a91776d5234b26e586630aba528105005ce5bf
Your Name <you@example.com>
2024-12-20 21:11:58 +0200

    server

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/welcome', function () {
     return view('welcome');
 });
 
