487efa900b2cbee767b41b2b9c68938b6d336027
maks <samper.goss@gmail.com>
2024-12-25 20:15:04 +0200

    + token api

---
+++ app/Http/Middleware/AuthenticateApi.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthenticateApi
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $token = $request->bearerToken();
+
+        if (!$token) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        if($token === config('apitoken')) {
+            return $next($request);
+        }
+    }
+}
