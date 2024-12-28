004666c9b9009a3f0a15e3c9887b1cfa490c74fc
maks <samper.goss@gmail.com>
2024-12-28 16:20:04 +0200

    + fix login and register

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,14 +4,34 @@ namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
-
-class Authenticate extends Middleware
+use Closure;
+use Illuminate\Support\Facades\Auth;
+class Authenticate
 {
     /**
-     * Get the path the user should be redirected to when they are not authenticated.
+     * Determine if the user is authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string|null  $guard
+     * @return mixed
      */
-    protected function redirectTo(Request $request): ?string
+    public function handle(Request $request, Closure $next, $guard = null)
     {
-        return $request->expectsJson() ? null : route('login');
+        if (Auth::guard($guard)->guest()) {
+            if ($request->expectsJson()) {
+                return response()->json(['message' => 'Unauthorized'], 401); // Повертаємо 401 для API запитів
+            }
+
+            return redirect()->route('login'); // Якщо не API-запит, перенаправляємо на сторінку входу
+        }
+
+        return $next($request);
     }
+
+    /**
+     * Get the path the user should be redirected to when not authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return string|null
+     */
 }
